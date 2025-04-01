af6f425a9449f5a681bec124b3569e5bea612c3e
WasabiThumb <wasabithumbs@gmail.com>
2025-04-01 09:50:18 -0400

    Setup packaging

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    id("io.github.goooler.shadow") version "8.1.8"
 }
 
 group = "com.github.springerris"
@@ -7,18 +8,27 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
-    mavenLocal()
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
     implementation("io.github.wasabithumb:yandisk4j:0.4.0")
     implementation("io.github.wasabithumb:magma4j:0.1.1")
     implementation("com.hierynomus:sshj:0.39.0")
     implementation("org.jetbrains:annotations:26.0.2")
 }
 
-tasks.test {
-    useJUnitPlatform()
+tasks.compileJava {
+    targetCompatibility = "21"
+    sourceCompatibility = "21"
+}
+
+tasks.shadowJar {
+    archiveClassifier.set("")
+    manifest {
+        attributes["Main-Class"] = "com.github.springerris.Main"
+    }
+}
+
+tasks.build {
+    dependsOn(tasks.shadowJar)
 }
